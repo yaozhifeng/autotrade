709c0d11540a5c7823a076c4e11b77070bf443f6
Neil Yao <yaozhifeng@gmail.com>
2025-03-12 10:05:33 +0800

    fix: correct fee calculation in StockGridTrader to accurately reflect total orders

--- us/us_grid.py
+++ us/us_grid.py
@@ -183,7 +183,7 @@ class StockGridTrader:
         avg_sell_price = self.daily_stats['total_sell_price'] / self.daily_stats['sell_orders'] if self.daily_stats['sell_orders'] > 0 else 0
 
         gross_margin = (avg_sell_price - avg_buy_price) * min(self.daily_stats['sell_orders'], self.daily_stats['buy_orders'])
-        fee = self.get_fee(self.quantity * (self.daily_stats['sell_orders'] + self.daily_stats['buy_orders']))
+        fee = self.get_fee(self.quantity) * (self.daily_stats['sell_orders'] + self.daily_stats['buy_orders'])
         net_profit = gross_margin - fee
 
         briefing_msg = (
